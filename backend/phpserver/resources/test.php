0f0ee21a00b978d9cfeb672f201c4f25262518db
Destinyr4zr <pristine.kirill@gmail.com>
2019-12-22 22:49:45 +0300

    Создаётся класс для теста

---
+++ backend/phpserver/resources/test.php
@@ -0,0 +1,15 @@
+<?php
+/*class Test
+{
+    public $name;
+    public $authorID;
+    public $questions = [];
+    function
+}
+
+class Question
+{
+    public $name;
+    public $answers =[];
+}
+*/?>
